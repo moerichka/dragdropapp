176b1aaf9fafa9b66420f066bbcf29e092d12f13
MichailI <mishika1221@mail.ru>
2022-07-13 16:44:48 +0500

    add some elements

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -3,7 +3,14 @@ import s from "./inputField.module.scss"
 
 const InputField : React.FC = () => {
   return (
-    <div>InputField</div>
+    <div className={s.inputField}>
+        <div className={s.container}>
+            <form className={s.form}>
+                <input type="text" placeholder='Add a new task' className={s.input} />
+                <button className={s.submit} type="submit">Go</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
