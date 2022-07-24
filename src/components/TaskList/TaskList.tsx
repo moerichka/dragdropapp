28fb76631fc88d319941005ee54eb68e842a0963
MichailI <mishika1221@mail.ru>
2022-07-24 17:24:03 +0500

    starting drag&drop implementation

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -4,7 +4,7 @@ import s from "./taskList.module.scss";
 import SingleTask from "../SingleTask";
 
 import { Task } from "../../model";
-import {Actions} from "../../reducer/tasksReducer"
+import { Actions } from "../../reducer/tasksReducer";
 
 interface Props {
   tasksState: Task[];
@@ -14,15 +14,33 @@ interface Props {
 const TaskList: React.FC<Props> = (props) => {
   return (
     <div className={s.taskList}>
-        <div className={s.container}>
-        {props?.tasksState?.map((task) => (
-          <SingleTask
-            task={task}
-            key={task.id}
-            tasksState={props.tasksState}
-            tasksDispatch={props.tasksDispatch}
-          />
-        ))}
+      <div className={s.container}>
+        <div className={s.active}>
+          <div className={s.taskheader}>Active Tasks</div>
+          <div className={s.tasks}>
+            {props?.tasksState?.map((task) => (
+              <SingleTask
+                task={task}
+                key={task.id}
+                tasksState={props.tasksState}
+                tasksDispatch={props.tasksDispatch}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={s.done}>
+          <div className={s.taskheader}>Completed Tasks</div>
+          <div className={s.tasks}>
+            {props?.tasksState?.map((task) => (
+              <SingleTask
+                task={task}
+                key={task.id}
+                tasksState={props.tasksState}
+                tasksDispatch={props.tasksDispatch}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
